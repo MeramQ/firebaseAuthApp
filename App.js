a5416aaad592acfd5707d999b97f90250d880849
Marcin Lesiak <marcinui07@gmail.com>
2025-02-20 08:43:04 +0100

    added necessary imports

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { db } from './firebaseConfig';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 
 export default function App() {
   return (
