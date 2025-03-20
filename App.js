016fb3d9d1f6eef6c5c40eef08edec185f36f3ee
Marcin Lesiak <marcinui07@gmail.com>
2025-03-20 09:10:31 +0100

    added registering new users after login and styled the app

--- App.js
+++ App.js
@@ -1,17 +1,23 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, TextInput, Alert, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, Alert, SafeAreaView, TouchableOpacity } from 'react-native';
 import { db } from './firebaseConfig';
-import { collection, query, where, getDocs } from 'firebase/firestore';
+import { collection, query, where, getDocs, addDoc } from 'firebase/firestore';
 
 export default function App() {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState('');
+  const [newFirstName, setNewFirstName] = useState('');
+  const [newLastName, setNewLastName] = useState('');
+  const [newLogin, setNewLogin] = useState('');
+  const [newEmail, setNewEmail] = useState('');
+  const [newPassword, setNewPassword] = useState('');
 
   const handleLogin = async () => {
     if (login === '' || password === '') {
       Alert.alert('Błąd', 'Uzupełnij wszystkie pola');
+      return;
     }
 
     try {
@@ -45,6 +51,21 @@ export default function App() {
     Alert.alert('Wylogowano', 'Zostałeś poprawnie wylogowany');
   }
 
+  const handleAddUser = async (email, firstName, lastName, login, password) => {
+    try {
+      await addDoc(collection(db, 'users'), {
+        email,
+        firstName,
+        lastName,
+        login,
+        password
+      });
+      Alert.alert('Dodano użytkownika', 'Użytkownik został dodany do bazy danych');
+    } catch (error) {
+      Alert.alert('Błąd', 'Wystąpił błąd, spróbuj ponownie później');
+    }
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       {!user ? (
@@ -52,12 +73,30 @@ export default function App() {
           <Text style={styles.title}>Logowanie</Text>
           <TextInput style={styles.input} placeholder="Podaj login" onChangeText={setLogin}></TextInput>
           <TextInput style={styles.input} placeholder="Podaj hasło" onChangeText={setPassword} secureTextEntry></TextInput>
-          <Button title="Zaloguj się" onPress={handleLogin}></Button>
+          <TouchableOpacity onPress={handleLogin} style={styles.button}>
+            <Text>Zaloguj się</Text>
+          </TouchableOpacity>
         </View>
       ) : (
-        <View>
-          <Text style={styles.title}>Witaj {user.firstName} {user.lastName}</Text>
-          <Button title="Wyloguj" onPress={handleLogout}></Button>
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.title}>Witaj, {user.firstName} {user.lastName}</Text>
+            <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+              <Text>Wyloguj się</Text>
+            </TouchableOpacity>
+          </View>
+
+          <View style={styles.registerContainer}>
+            <Text style={styles.title}>Dodaj nowego użytkownika</Text>
+            <TextInput style={styles.input} placeholder="Podaj imię nowego użytkownika" onChangeText={setNewFirstName} />
+            <TextInput style={styles.input} placeholder="Podaj nazwisko nowego użytkownika" onChangeText={setNewLastName} />
+            <TextInput style={styles.input} placeholder="Podaj email nowego użytkownika" onChangeText={setNewEmail} />
+            <TextInput style={styles.input} placeholder="Podaj login nowego użytkownika" onChangeText={setNewLogin} />
+            <TextInput style={styles.input} placeholder="Podaj hasło nowego użytkownika" onChangeText={setNewPassword} secureTextEntry />
+            <TouchableOpacity style={styles.button} onPress={() => handleAddUser(newEmail, newFirstName, newLastName, newLogin, newPassword)}>
+              <Text>Dodaj użytkownika</Text>
+            </TouchableOpacity>
+          </View>
         </View>
       )}
     </SafeAreaView>
@@ -67,22 +106,51 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FAF1E6',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  registerContainer: {
+    marginTop: 10
   },
   title: {
     fontSize: 24,
     marginBottom: 24,
+    textAlign: 'center',
+    marginTop: 50
   },
   input: {
+    backgroundColor: '#FDFAF6',
     borderColor: 'gray',
     borderWidth: 1,
     borderRadius: 5,
-    width: 200,
+    width: 275,
     height: 40,
     padding: 10,
     marginBottom: 10,
-  }
+  },
+  button: {
+    backgroundColor: '#99BC85',
+    padding: 10,
+    borderRadius: 5,
+    width: 275,
+    alignItems: 'center',
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+  },
+  logoutButton: {
+    backgroundColor: '#BF3131',
+    padding: 10,
+    borderRadius: 5,
+    width: 275,
+    alignItems: 'center',
+    marginTop: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 5,
+  },
 
 });
